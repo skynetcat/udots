26df4a312279a5359c983d70365a379de2081fff
skynetcat <188181928+skynetcat@users.noreply.github.com>
2025-05-25 00:58:31 +0300

    Added backup option

--- config/udots-config.sh
+++ config/udots-config.sh
@@ -13,3 +13,15 @@ ADOPT=0
 
 # Command used to erase packages
 ERASE_CMD="rm -rf"
+
+# Backup files before moving them into pkg repo
+#   0 => No backup, just move
+#   1 => Backup before moving
+BACKUP=1
+
+# Backup directory:
+#   same => backup in the same directory
+#   /path/to/backup => backup to a certain path
+#
+# Backups of the same file will not be replaced so don't worry
+BACKUP_DIR="same"
